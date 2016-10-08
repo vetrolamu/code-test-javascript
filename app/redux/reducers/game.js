2356afb643a4f60a3d39074b726d64fef9bafb7d
vetrolamu <gerasimenko.ab@gmail.com>
2016-10-08 15:02:26 +0300

    [fix] Use module for updating of results

--- app/redux/reducers/game.js
+++ app/redux/reducers/game.js
@@ -12,7 +12,73 @@ const initialState = {
     results: [],
     maxScore: GAME_CONFIG.pins
 };
-let waitingList = [];
+
+const resultsUpdater = (function() {
+    let waitingList = [];
+
+    function updateFromWaitingList(results, score) {
+        waitingList = waitingList
+            .map(item => {
+                results[item.index].score += score;
+                return {index: item.index, times: item.times - 1};
+            })
+            .filter(({times}) => times > 0);
+
+        return results;
+    }
+
+    return {
+        /**
+         * Updates array of results by new score. And returns updated array.
+         * @param {Array} oldResults
+         * @param {Number} score
+         * @param {Number} index
+         * @returns {Array}
+         */
+        update(oldResults, score, index) {
+            let results = oldResults.slice();
+            const isFirstRollInFrame = !results[index];
+
+            results = updateFromWaitingList(results, score);
+            results[index] = results[index] || {rolls: [], score: 0};
+            results[index].rolls.push(score);
+            results[index].score += score;
+
+            // last frame
+            if (index === GAME_CONFIG.frames - 1) {
+                const rollsNumber = results[index].rolls.length;
+
+                results[index].closed = (
+                    rollsNumber === 3 ||
+                    rollsNumber === 2 && results[index].score < GAME_CONFIG.pins
+                );
+
+                return results;
+            }
+
+            if (isFirstRollInFrame) {
+                // strike
+                if (score === GAME_CONFIG.pins) {
+                    waitingList.push({index, times: 2});
+                    results[index].closed = true;
+                }
+                return results;
+            }
+
+            // spare
+            if (results[index].score === GAME_CONFIG.pins) {
+                waitingList.push({index, times: 1});
+            }
+
+            results[index].closed = true;
+
+            return results;
+        },
+        clear() {
+            waitingList = [];
+        }
+    };
+}());
 
 function getMaxScore(frame) {
     if (frame && (frame.score % GAME_CONFIG.pins)) {
@@ -37,56 +103,6 @@ function isScoreValid(score, maxScore) {
     return numericScore >= 0 && numericScore <= maxScore;
 }
 
-/**
- * Updates array of results by new score. And returns updated array.
- * @param {Array} oldResults
- * @param {Number} score
- * @param {Number} index
- * @returns {Array}
- */
-function updateResults(oldResults, score, index) {
-    const results = oldResults.slice();
-    const isFirstRollInFrame = !results[index];
-
-    waitingList = waitingList
-        .map(item => {
-            results[item.index].score += score;
-            return {index: item.index, times: item.times - 1};
-        })
-        .filter(({times}) => times > 0);
-
-    results[index] = results[index] || {rolls: [], score: 0};
-    results[index].rolls.push(score);
-    results[index].score += score;
-
-    // last frame
-    if (index === GAME_CONFIG.frames - 1) {
-        const rollsNumber = results[index].rolls.length;
-
-        results[index].closed = rollsNumber === 3 || rollsNumber === 2 && results[index].score < GAME_CONFIG.pins;
-
-        return results;
-    }
-
-    if (isFirstRollInFrame) {
-        // strike
-        if (score === GAME_CONFIG.pins) {
-            waitingList.push({index, times: 2});
-            results[index].closed = true;
-        }
-        return results;
-    }
-
-    // spare
-    if (results[index].score === GAME_CONFIG.pins) {
-        waitingList.push({index, times: 1});
-    }
-
-    results[index].closed = true;
-
-    return results;
-}
-
 function getCurrentFrameIndex(results) {
     const lastIndex = results.length - 1;
     const lastResult = results[lastIndex];
@@ -109,7 +125,7 @@ export default (state=initialState, action) => {
                 return {...state, currentScoreError: 'Wrong value'};
             }
 
-            const results = updateResults(state.results, Number(state.currentScore), state.currentFrameIndex);
+            const results = resultsUpdater.update(state.results, Number(state.currentScore), state.currentFrameIndex);
             const currentFrameIndex = getCurrentFrameIndex(results);
             const maxScore = getMaxScore(results[currentFrameIndex]);
 
@@ -119,7 +135,7 @@ export default (state=initialState, action) => {
             return {...state, currentScore: action.score, currentScoreError: null};
 
         case constants.RESTART_GAME:
-            waitingList = [];
+            resultsUpdater.clear();
 
             return {...initialState};
 
