fd39cc07859532d6e600a644a56f86b66dfed3bb
vetrolamu <gerasimenko.ab@gmail.com>
2016-10-10 02:42:25 +0300

    [update] Styling of buttons and inputs

--- app/components/button/button.jsx
+++ app/components/button/button.jsx
@@ -1,9 +1,18 @@
+import b from 'b_';
 import React from 'react';
 
 import './button.scss';
 
 const Button = (props) => (
-    <button className="button" {...props} />
+    <button className={b('button', {size: props.size})} {...props} />
 );
+
+Button.defaultProps = {
+    size: 'm'
+};
+
+Button.propTypes = {
+    size: React.PropTypes.string
+};
 
 export default Button;
